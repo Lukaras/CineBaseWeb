eab5596e2ae060e15b8d130dd76ef7c6851c943c
lukaraslolcz <lukaraslolcz@gmail.com>
2019-04-02 22:32:02 +0200

    Login Added

--- CineBase/Controllers/UserController.cs
+++ CineBase/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -30,9 +31,49 @@ namespace CineBase.Controllers
             manager.Add(model);
         }
 
-        public void Login(UserViewModel model)
+        public ActionResult _Login(UserViewModel model)
         {
-            manager.Login(model);
+
+            var result = Procced(model.Username, model.Password);
+            if (result == "ok")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else ModelState.AddModelError("Login", result);
+
+            return View(model);
+        }
+
+        private string Procced(string login, string password)
+        {
+
+            var entity = new User();
+
+            string query = string.Format("SELECT [Id], [Username], [Password], [PasswordSalt] FROM [User] WHERE [Username] = '{0}'", login);
+            SqlCommand cmd = new SqlCommand(query, Database.db);
+            SqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            entity = new User
+            {
+                Id = reader.GetInt32(0),
+                Username = reader.GetString(1),
+                Password = reader.GetString(2),
+            };
+            string salt = reader.GetString(3);
+
+            if (entity == null) return "Uživatel neexistuje.";
+
+            var encrypted = Helper.Hashing(password + salt);
+            encrypted = encrypted.Replace('�', '?');
+            if (entity.Password == encrypted)
+            {
+
+                var expire = DateTime.Now.AddMinutes(240);
+
+                Response.Cookies.Append("user", entity.Id.ToString());
+                return "ok";
+            }
+            return "Přihlašovací údaje nejsou správné.";
         }
     }
 }
--- CineBase/Managers/Shared/Helper.cs
+++ CineBase/Managers/Shared/Helper.cs
@@ -9,18 +9,19 @@ namespace CineBase
     public class Helper
     {
 
-	private static Random rng = new Random();
-	public static string Hashing(string input)
-	{	    
-	    return System.Text.Encoding.UTF8.GetString(SHA256.Create().ComputeHash(System.Text.Encoding.UTF8.GetBytes(input)));
-	}
+        private static Random rng = new Random();
 
-	public static string RandomString(int length)
-	{
-	    const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-	    return new string(Enumerable.Repeat(chars, length)
-	      .Select(s => s[rng.Next(s.Length)]).ToArray());
-	}
+        public static string Hashing(string input)
+        {
+            return System.Text.Encoding.UTF8.GetString(SHA256.Create().ComputeHash(System.Text.Encoding.UTF8.GetBytes(input)));
+        }
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[rng.Next(s.Length)]).ToArray());
+        }
 
     }
 }
